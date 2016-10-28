21dbf268abd8bb5c1d73c594756eda20c27128d8
João Vieira <2121089@my.ipleiria.pt>
2016-10-28 14:04:51 +0100

    jkk

--- AdinistrationApp_eNutre/Classes/TxtHandler.cs
+++ AdinistrationApp_eNutre/Classes/TxtHandler.cs
@@ -3,16 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
 using AdministrationApp_eNutre.Classes;
 
 namespace AdinistrationApp_eNutre.Classes
 {
     class TxtHandler
     {
-        private List<string> vegetal;
+       // private List<string> vegetal;
         private string[] vegetalSplit;
         private string[] vegetalSplitP;
         private string[] vegetalSplitComma;
+        private string[] vegetables;
 
         public void carregarTXT()
         {
@@ -20,14 +22,14 @@ namespace AdinistrationApp_eNutre.Classes
                 System.IO.File.ReadAllText(
                     @"C:\Users\j17vi\Source\Repos\App_eNutre\AdinistrationApp_eNutre\Info\calorias_vegetais.txt");
 
-            string[] vegetables = ficheiroTXT.Split('\n');
+            vegetables = ficheiroTXT.Split('\n');
             // Vegetable vegetable = new Vegetable(vegetables); 
-
             for (int i = 0; i < vegetables.Length; i++)
             {
                 vegetalSplit = vegetables[i].Split(' ');
                 vegetalSplitP = vegetables[i].Split('(');
-            }  
+            }
+
         }
 
         public string devolveNome()
@@ -67,20 +69,49 @@ namespace AdinistrationApp_eNutre.Classes
             else
             {
                 return vegetalSplitP[1];
-            }
+            }  
+        }
+
+        private static void createXml(string[] vegetables, string[] vegetableSplitP, string[] vegetableSplitComma)
+        {
+
+            XmlDocument doc = new XmlDocument();
+
+            XmlDeclaration dec = doc.CreateXmlDeclaration("1.0", null, null);
+            doc.AppendChild(dec);
+
+            // ROOT
+            XmlElement root = doc.CreateElement("foods");
+            doc.AppendChild(root);
             
-        }
-        /*
-        public string[] devolveVegetalLista()
-        {
-            return vegetalSplit;
+           
+
+            for (int i = 0; i < vegetables.Length ; i++)
+            {
+                XmlElement food = doc.CreateElement("food");
+                XmlElement vegetable = doc.CreateElement("vegetable");
+                //food.InnerText = 
+                XmlElement name = doc.CreateElement("name");
+                XmlElement extraInfo = doc.CreateElement("extraInfo");
+                XmlElement quantity = doc.CreateElement("quantity");
+                XmlElement calories = doc.CreateElement("calories");
+
+
+                //activity.InnerText = act.Nome;
+
+                // met.InnerText = act.Met.ToString();
+
+                // calories.InnerText = act.Calorias.ToString();
+
+                /*
+                exercise.AppendChild(activity);
+                exercise.AppendChild(met);
+                exercise.AppendChild(calories);
+                root.AppendChild(exercise);*/
+            }
+            doc.Save(@"..\\..\\XML\\Xml_Files\\vegetables.xml");
         }
 
-        public string[] devolveVegetalListaP()
-        {
-            return vegetalSplitP;
-        }*/
 
-       
     }
 }
