9f53adae998684bcceda1da7fdcc991bfa281760
Gone4Coding <gongas.gordalina14@gmail.com>
2016-10-28 11:02:39 +0100

    nova classe

---
+++ AdinistrationApp_eNutre/Classes/JsonHandler.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing.Text;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AdministrationApp_eNutre.Classes;
+using Newtonsoft.Json;
+
+namespace AdinistrationApp_eNutre.Classes
+{
+    class JsonHandler
+    {
+        private void deserialize(string jsonFile, string type)
+        {
+            //type; // Act; Res; Veg; 
+
+            string json = System.IO.File.ReadAllText(jsonFile);
+            switch (type)
+            {
+                case "Act":
+                    List<Activity> activitiesList = new List<Activity>();
+                    activitiesList = JsonConvert.DeserializeObject<List<Activity>>(json);
+                    createXmlFile();
+                    break;
+                case "Res":
+                    List<Restaurant> restaurantsList = new List<Restaurant>();
+                    restaurantsList = JsonConvert.DeserializeObject<List<Restaurant>>(json);
+                    createXmlFile();
+                    break;
+                case "Veg":
+                    List<Vegetable> vegetablesList = new List<Vegetable>();
+                    vegetablesList = JsonConvert.DeserializeObject<List<Vegetable>>(json);
+                    createXmlFile();
+                    break;
+            }
+        }
+
+        private void createXmlFile()
+        {
+            
+        }
+    }
+}
