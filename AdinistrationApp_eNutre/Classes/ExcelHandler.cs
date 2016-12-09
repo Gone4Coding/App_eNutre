4a1c372d467646ec5d3b18752a5d95588c2d3998
Gone4Coding <gongas.gordalina14@gmail.com>
2016-12-09 13:00:37 +0000

    Excel

--- AdinistrationApp_eNutre/Classes/ExcelHandler.cs
+++ AdinistrationApp_eNutre/Classes/ExcelHandler.cs
@@ -7,6 +7,7 @@ using System.Windows.Forms;
 using System.Xml;
 using System.Xml.Schema;
 using AdinistrationApp_eNutre.Forms;
+using AdministrationApp_eNutre.Classes;
 using Excel = Microsoft.Office.Interop.Excel;
 
 
@@ -19,7 +20,6 @@ namespace AdinistrationApp_eNutre.Classes
             bool isValid = true;
             string msg1 = "";
             string msg2;
-           
 
             Excel.Application excelApplication = new Excel.Application();
             excelApplication.Visible = false;
@@ -35,30 +35,29 @@ namespace AdinistrationApp_eNutre.Classes
 
                 XmlElement root = doc.CreateElement("foods");
                 doc.AppendChild(root);
-
+                
                 for (int line = 2; line <= workSheet.UsedRange.Rows.Count; line++)
                 {
-                    XmlElement food = doc.CreateElement("food");
+                    string restaurantName = workSheet.Cells[line, 1].Value;
 
-                    XmlElement restaurant = doc.CreateElement("restaurant");
-                    restaurant.InnerText = workSheet.Cells[line, 1].Value;
+                    XmlElement restaurantNode = doc.CreateElement("restaurant");
+                    restaurantNode.SetAttribute("name", restaurantName);
 
-                    XmlElement item = doc.CreateElement("item");
-                    item.InnerText = workSheet.Cells[line, 2].Value;
+                    XmlElement plateNode = getPlate(line, path);
+                    restaurantNode.AppendChild(plateNode);
 
-                    XmlElement quantity = doc.CreateElement("quantity");
-                    quantity.InnerText = workSheet.Cells[line, 3].Value;
+                    for(int lineAux = line + 1; lineAux <= workSheet.UsedRange.Rows.Count; lineAux++)
+                    {
+                        string restaurantNameAux = workSheet.Cells[lineAux, 1].Value;
 
-                    XmlElement calories = doc.CreateElement("calories");
-                    string cal = workSheet.Cells[line, 4].Value;
-                    string[] split = cal.Split(' ');
-                    calories.InnerText = split[0];
+                        if (restaurantNameAux.Equals(restaurantName))
+                        {
+                            XmlElement plateNodeAux = getPlate(lineAux, path);
+                            restaurantNode.AppendChild(plateNodeAux);
 
-                    food.AppendChild(restaurant);
-                    food.AppendChild(item);
-                    food.AppendChild(quantity);
-                    food.AppendChild(calories);
-                    root.AppendChild(food);
+                            line++;
+                        }
+                    }
                 }
                 
                 doc.Save(@"..\\..\\XML\\Xml_Files\\restaurants.xml");
@@ -89,6 +88,63 @@ namespace AdinistrationApp_eNutre.Classes
             return msg2;
         }
 
+        private static XmlElement getPlate(int line, string path)
+        {
+            Excel.Application excelApplication = new Excel.Application();
+            excelApplication.Visible = false;
+
+            Excel.Workbook workBook = excelApplication.Workbooks.Open(path);
+            Excel.Worksheet workSheet = workBook.Worksheets.get_Item(1);
+
+            XmlDocument doc = new XmlDocument();
+            XmlElement plateNode = doc.CreateElement("plate");
+
+            XmlElement itemNode = doc.CreateElement("item");
+            itemNode.InnerText = workSheet.Cells[line, 2].Value;
+
+            XmlElement quantityNode = doc.CreateElement("quantity");
+
+            string q = workSheet.Cells[line, 3].Value;
+            string[] quantityArray = q.Split(' ');
+
+            XmlElement quatityValue = doc.CreateElement("value");
+            quatityValue.InnerText = quantityArray[0];
+            quantityNode.AppendChild(quatityValue);
+
+            XmlElement quantityDosage = doc.CreateElement("dosage");
+            quantityDosage.InnerText = quantityArray[1];
+            quantityNode.AppendChild(quantityDosage);
+
+            if (!quantityArray[3].Equals(""))
+            {
+                string[] noLeftParentsis = quantityArray[3].Split('(');
+                string[] noRightParentsis = noLeftParentsis[0].Split(')');
+                string extraDosage = noRightParentsis[0];
+
+                XmlElement quantityExtraDosage = doc.CreateElement("extraDosage");
+                quantityExtraDosage.InnerText = extraDosage;
+                quantityNode.AppendChild(quantityExtraDosage);
+            }
+
+            XmlElement caloriesNode = doc.CreateElement("calories");
+
+            string c = workSheet.Cells[line, 4].Value;
+            string[] caloriesArray = c.Split(' ');
+
+            XmlElement caloriesValue = doc.CreateElement("value");
+            caloriesValue.InnerText = caloriesArray[0];
+            caloriesNode.AppendChild(caloriesValue);
+
+            XmlElement caloriesUnity = doc.CreateElement("unity");
+            caloriesUnity.InnerText = caloriesArray[1];
+            caloriesNode.AppendChild(caloriesUnity);
+
+            plateNode.AppendChild(itemNode);
+            plateNode.AppendChild(quantityNode);
+            plateNode.AppendChild(caloriesNode);
+
+            return plateNode;
+        }
 
     }
 }
